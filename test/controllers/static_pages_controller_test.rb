2d964bc511d1a8ae0caaeffa89e5bdc020c9a37c
IwanagaSari <catherinrin@gmail.com>
2019-10-23 21:48:28 +0900

    Contactページテストすでに自分で追加してたので１つ削除

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -34,10 +34,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
-
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
-  end
 end
