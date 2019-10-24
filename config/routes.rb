4d84bcc0a2edd44c0cf7df7819baa5a14707c652
岩永彩里 <catherinrin@gmail.com>
2019-10-24 15:40:26 +0900

    helfルーティングを作成し、レイアウトのリンクを更新してみる

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   
-  get '/help', to: 'static_pages#help'
+  get '/help', to: 'static_pages#help', as: 'helf'
 
   get '/about', to: 'static_pages#about'
 
