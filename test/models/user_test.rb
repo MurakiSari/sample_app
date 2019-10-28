c112ed3c8d7cf59a769376662c17ccd61703e2eb
IwanagaSari <catherinrin@gmail.com>
2019-10-28 16:48:07 +0900

    重複するメールアドレス拒否するテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,4 +46,10 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_use = @user.dup
+    @user.save
+    assert_not duplicate_use.valid?
+  end
 end
