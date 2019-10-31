b3424f5634748c235f5782a5ac978cea180a1845
IwanagaSari <catherinrin@gmail.com>
2019-10-31 11:45:04 +0900

    redirect_to @userをredirect_to user_url(@user)が同じ機能であることを確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to user_url(@user)
+      #redirect_to @user
     else
       render 'new'
     end
