89f04745ebda4839c3b578b0c4e32485824b2d34
IwanagaSari <catherinrin@gmail.com>
2019-10-31 12:39:20 +0900

    ユーザーを登録できた時、flashのメッセージを表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
       #redirect_to @user
     else
