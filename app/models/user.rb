1e313fc51b1587474aa16a095f8a0278b01e2a86
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-05 18:28:42 +0900

    渡された文字列のハッシュ値を返す　digestメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
               format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
