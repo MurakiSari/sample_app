2f5088b2818cef556badbbdbff1ddf35b30b8be3
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-06 11:42:53 +0900

    =begin, =endを使ってまとめてコメントアウトしてみた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+=begin
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
@@ -25,3 +26,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+=end
