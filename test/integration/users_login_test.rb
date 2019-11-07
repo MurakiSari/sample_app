e8e5448da91140c3aba8705da0f28f20371314fc
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 12:03:29 +0900

    2番目のウィンドウでログアウトした時をシュミレートするテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,7 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: {email: "", password: ""} }
+    post login_path, params: { session: { email: "", password: "" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -17,7 +17,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -28,7 +28,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password'} }
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -39,6 +39,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+
+    # 2番目のウィンドウでもログアウト
+    delete logout_path
+
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
