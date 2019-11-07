30ba8318d184ed2f061c577f48db0d6c8decd9d4
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 10:09:38 +0900

    rememberスペルミスしてました...ので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  def remenber
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
