81ea1ea4de8add3938e5973836409bfd429fee34
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 11:33:40 +0900

    Sessionヘルパーのメソッドを少し並び替えた

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,6 +19,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def log_out
     forget(current_user)
     session.delete(:user_id)
@@ -29,11 +35,5 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
   end
 end
