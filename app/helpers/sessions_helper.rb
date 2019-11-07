3fd524380530ebb2c9590188e0ae4f37294e0033
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 11:33:02 +0900

    Sessionヘルパーに永続的セッションを破棄するforgetメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,8 +20,15 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   def remember(user)
