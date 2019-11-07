f941a49c3989b73bc64016f7c603842c093a331e
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 14:26:26 +0900

    rememberをチェックしたら、cookiesに保存されているかのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -48,4 +48,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '2')
+    assert_empty cookies['remember_token']
+  end
 end
