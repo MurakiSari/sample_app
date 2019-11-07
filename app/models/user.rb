b5edb34157a2a5caa0d213244ccc5f6aa86771d5
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-07 17:17:23 +0900

    スペルミスを修正(passwordが...)

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     return false if remember_digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_passwoed?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   def forget
