ae8a24862f08619e5b7ce3a727eff46a5eb454a7
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-08 10:46:33 +0900

    returnを使って条件分のネストを解消

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,10 +8,11 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user&.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+
+      return unless user&.authenticated?(cookies[:remember_token])
+
+      log_in user
+      @current_user = user
     end
   end
 
