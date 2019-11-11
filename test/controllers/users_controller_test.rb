056421a4313e27cb53435be44844ddecd3dd92fb
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-11 16:30:25 +0900

    admin属性を変更できないことを確認するテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: @other_user.password,
+                                                    password_confirmation: @other_user.password,
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
