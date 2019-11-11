dfa58fa743d38b4b77725de0c87d17d69519ce5a
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-11 10:26:35 +0900

    ログイン後のリダイレクト先を変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user&.authenticate(session_params[:password])
       log_in @user
       session_params[:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
