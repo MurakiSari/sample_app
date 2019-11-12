3115f4fcc9f182043c3a6f46d622534ea9bbd989
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-12 17:15:32 +0900

    Userメイラーの生成 $ rails generate mailer UserMailer account_activation password_reset

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+  def account_activation
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def password_reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
