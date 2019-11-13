e313fdc9aa7176bd8cf0a9dddc5ebe0f5235ccb1
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-13 11:54:51 +0900

    userを@userに修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: session_params[:email].downcase)
     if @user&.authenticate(session_params[:password])
-      if user.activated?
+      if @user.activated?
         log_in @user
         session_params[:remember_me] == '1' ? remember(@user) : forget(@user)
         redirect_back_or @user
