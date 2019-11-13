c0dd038ec629c2044fe4db0eb0ef1a2cd3e25dc4
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-13 12:00:44 +0900

    Userモデルにユーザー有効化メソッドを追加し、リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,15 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def downcase_email
