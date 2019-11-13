85394c529f85ddfa0d6b4a5755e32c0f15728f6f
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-13 10:36:08 +0900

    sessionコントローラーのcreateメソッドに、有効なユーザーかどうか確認する処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: session_params[:email].downcase)
     if @user&.authenticate(session_params[:password])
-      log_in @user
-      session_params[:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if user.activated?
+        log_in @user
+        session_params[:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        message = "Account not activated. Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
