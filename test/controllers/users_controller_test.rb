77f1df87e9ad5a759f4fbdeef813ca47446a7918
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-13 12:33:15 +0900

    無効なユーザーは一覧画面で非表示になってるか＋showページを表示しようとするとトップ画面にリダイレクトしているか、のテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @non_activated_user = users(:non_activated)
   end
 
   test "should redirect index when not logged in" do
@@ -65,6 +66,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
+    assert_redirected_to root_url
+  end
+
+  test "should not allow the not activated attribute" do
+    log_in_as(@non_activated_user)
+    assert_not @non_activated_user.activated?
+    get users_path
+    assert_select "a[href=?]", user_path(@non_activated_user), count: 0
+    get user_path(@non_activated_user)
     assert_redirected_to root_url
   end
 end
