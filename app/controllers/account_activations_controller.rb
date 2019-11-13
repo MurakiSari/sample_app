18dfa52c3f5a9170b92708e84f95addeb5499a3e
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-13 12:08:27 +0900

    呼び出す時のメソッド名間違えてたので修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,7 +2,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.active
+      user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
