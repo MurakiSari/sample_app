1fd8572df33fcbaa35d15e28dc8986e50647b1af
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-14 11:57:51 +0900

    Userモデルにカラムを追加した（うまく追加できなくて少しハマった）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191114004731) do
+ActiveRecord::Schema.define(version: 20191114022505) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20191114004731) do
     t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
