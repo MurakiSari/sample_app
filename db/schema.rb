af582cd2c361f5acbf07f1ec3eb37f465866b9c0
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-15 16:59:58 +0900

    user_idとcreated_atカラムにインデックスを付与 $rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191114022505) do
+ActiveRecord::Schema.define(version: 20191115060924) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", nil], name: "index_microposts_on_user_id_and_creates_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
