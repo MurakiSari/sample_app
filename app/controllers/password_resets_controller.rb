63aa0710f859149d6f13d353728fcdd150e9a999
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-15 10:18:27 +0900

    ここでテストこけてた部分を（no method error）修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,7 +50,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def valid_user
-    unless @user&.activated?&.authenticated?(:reset, params[:id])
+    unless @user&.activated? && @user&.authenticated?(:reset, params[:id])
       redirect_to root_url
     end
   end
