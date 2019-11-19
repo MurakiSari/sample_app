0becbf142dac2841ab09e815d108df7881e10fd7
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-19 10:49:29 +0900

    ルーティングにマイクロポストリソースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
 
   resources :password_resets, only: %i(new create edit update)
+
+  resources :microposts, only: %i(create destroy)
 end
