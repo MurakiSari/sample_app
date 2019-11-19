1cbd5c0b824b95d9c69acbbc09422a82406c13cd
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-19 18:24:28 +0900

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115060924) do
+ActiveRecord::Schema.define(version: 20191119075311) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", nil], name: "index_microposts_on_user_id_and_creates_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
