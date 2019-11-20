a23e3b60a234b24bb1ef862684bc8193a7cb3219
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-20 10:28:30 +0900

    画像に対するバリデーションを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,13 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate { :picture_size }
+
+  private
+
+  def picture_size
+    if picture_size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
