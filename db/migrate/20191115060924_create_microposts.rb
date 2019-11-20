51f8ec642ffc0f14f867b03efe94db9686d7485a
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-20 12:15:47 +0900

    カラムの名前が間違っていた( 驚愕 )ので修正し、 $ bundle exec rake db:migrate:reset $ bundle exec rake db:migrate

--- db/migrate/20191115060924_create_microposts.rb
+++ db/migrate/20191115060924_create_microposts.rb
@@ -6,6 +6,6 @@ class CreateMicroposts < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-    add_index :microposts, %i(user_id creates_at)
+    add_index :microposts, %i(user_id created_at)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20191119075311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
-    t.index ["user_id", nil], name: "index_microposts_on_user_id_and_creates_at"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
