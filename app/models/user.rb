ffa5ca5d1a3939c4db732644d2190e1583cd4506
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-20 18:38:14 +0900

    "following" 関連のメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,18 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   private
 
   def downcase_email
