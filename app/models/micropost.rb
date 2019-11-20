32d16da62c325ba6a083c633df5dff698e01a939
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-20 17:50:00 +0900

    { }で囲んでいたバリデーションを修正（そもそもif文が違ってた）

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,12 +4,12 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate { :picture_size }
+  validate :picture_size
 
   private
 
   def picture_size
-    if picture_size > 5.megabytes
+    if picture.size > 5.megabytes
       errors.add(:picture, "should be less than 5MB")
     end
   end
