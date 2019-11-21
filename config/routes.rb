b3fbeecb1fcfb4e1c95d5c76ef828f2b00cc5bd6
muraki.sari <muraki.sari@moneyforward.co.jp>
2019-11-21 11:36:13 +0900

    Usersコントローラにfollowingアクションとfollowersアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,11 @@ Rails.application.routes.draw do
 
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :account_activations, only: [:edit]
 
